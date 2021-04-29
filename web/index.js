18ade7656e2b194725e5087048b72b8cda6691aa
Yash Diniz <yashdiniz@gmail.com>
2021-04-29 12:57:51 +0530

    Added support to 2 motors. Motor(12, 11) -> 1 Motor(10, 9) -> 2

--- web/index.js
+++ web/index.js
@@ -14,7 +14,10 @@ const board = new Board();
 
 board.on("ready", () => {
     console.log('Board ready');
-    const motor1 = new Motor(12, 11);
+    const m1 = { stepPin: 12, dirPin: 11, event: 'motor1:step' };
+    const m2 = { stepPin: 10, dirPin: 9,  event: 'motor2:step' };
+    const motor1 = new Motor(m1.stepPin, m1.dirPin);
+    const motor2 = new Motor(m2.stepPin, m2.dirPin);
     let idConnected = ""; // holds the ID of the active room...
 
     io.on('connection', socket => {
@@ -22,10 +25,16 @@ board.on("ready", () => {
         else socket.disconnect();   // disconnect every subsequent request which does not match
         console.log(socket.id, 'connected:', socket.connected, 'at', socket.conn.remoteAddress);
 
-        socket.on('motor1:step', (data) => {
-            console.log(socket.id, 'motor1:step', data);
+        socket.on(m1.event, (data) => {
+            console.log(socket.id, m1.event, data);
             if (data === 'cw') motor1.step(1);
             if (data === 'ccw') motor1.step(-1);
+        });
+
+        socket.on(m2.event, (data) => {
+            console.log(socket.id, m2.event, data);
+            if (data === 'cw') motor2.step(1);
+            if (data === 'ccw') motor2.step(-1);
         });
 
         socket.on('disconnect', () => {
